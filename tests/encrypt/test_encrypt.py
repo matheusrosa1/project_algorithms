b0f172f9090c6ef496ee1bd3594f2a818d3d4b75
Matheus Rosa <matheusrosataxa@gmail.com>
2024-05-09 18:00:00 -0300

    req. 2 finalizado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Hello", "invalid_key")
+
+    # Testa com mensagem inválida
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(12345, 2)
+
+    assert encrypt_message("XABLAU", 2) == "UALB_AX"
+    assert encrypt_message("Hello", 10) == "olleH"
+    assert encrypt_message("Hello", 4) == "o_lleH"
+    assert encrypt_message("Hello", 3) == "leH_ol"
