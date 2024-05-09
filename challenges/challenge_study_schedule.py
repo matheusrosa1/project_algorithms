2b9f6efb113c3085f7e97c7563bd23abea030e50
Matheus Rosa <matheusrosataxa@gmail.com>
2024-05-09 16:31:35 -0300

    req. 1 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,23 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        counter = 0
+        for entry_time, exit_time in permanence_period:
+            if entry_time <= target_time <= exit_time:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
+
+
+"""     try:
+        return sum(
+            entry_time <= target_time <= exit_time
+            for entry_time, exit_time in permanence_period
+        )
+    except TypeError:
+        return None """
+
+""" permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+target_time = 5
+print(study_schedule(permanence_period, target_time))
+ """
