1bb4973774c78e6267a7e4599e13fc87c33ea094
Matheus Rosa <matheusrosataxa@gmail.com>
2024-05-10 14:34:24 -0300

    req. 4 em andamento

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,32 +1,42 @@
-def merge_sort(array: list[str]) -> list[str]:
-    if len(array) <= 1:
-        return array
+def merge_sort(word: str) -> str:
+    if len(word) <= 1:
+        return word
 
-    mid = len(array) // 2
-    left = merge_sort(array[:mid])
-    right = merge_sort(array[mid:])
+    mid = len(word) // 2
+    left = merge_sort(word[:mid])
+    right = merge_sort(word[mid:])
 
-    return merge(left, right, array)
+    return merge(left, right, word)
 
 
 def merge(left: list[str], right: list[str], merged: list[str]) -> list[str]:
+    merged = []
     left_cursor, right_cursor = 0, 0
 
     while left_cursor < len(left) and right_cursor < len(right):
         if left[left_cursor] <= right[right_cursor]:
-            merged[left_cursor + right_cursor] = left[left_cursor]
+            merged.append(left[left_cursor])
             left_cursor += 1
         else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
+            merged.append(right[right_cursor])
             right_cursor += 1
 
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left[left_cursor]
+    merged.extend(left[left_cursor:])
+    merged.extend(right[right_cursor:])
 
-    for right_cursor in range(right_cursor, len(right)):
-        merged[left_cursor + right_cursor] = right[right_cursor]
-
-    return merged
+    return "".join(merged)
 
 
-def is_anagram(first_string, second_string): ...
+def verifyng_is_anagram(first_string, second_string): ...
+
+
+def is_anagram(first_string, second_string):
+    merged_first_string = merge_sort(first_string).lower()
+    merged_second_string = merge_sort(second_string).lower()
+    if merged_first_string == merged_second_string:
+        return (merged_first_string, merged_second_string, True)
+    else:
+        return False
+
+
+print(is_anagram("Amor", "roma"))
