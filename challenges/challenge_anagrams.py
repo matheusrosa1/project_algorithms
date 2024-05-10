278f08beaab4b5c3834803e69b1203a91c042d31
Matheus Rosa <matheusrosataxa@gmail.com>
2024-05-10 00:11:21 -0300

    req. 4 em andamento

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,32 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui.."""
-    raise NotImplementedError
+def merge_sort(array: list[str]) -> list[str]:
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    return merge(left, right, array)
+
+
+def merge(left: list[str], right: list[str], merged: list[str]) -> list[str]:
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+def is_anagram(first_string, second_string): ...
