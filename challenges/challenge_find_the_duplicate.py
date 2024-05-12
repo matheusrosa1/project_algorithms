a02393fed990259a590a39c2bedc9c8bf43b4570
Matheus Rosa <matheusrosataxa@gmail.com>
2024-05-11 22:02:19 -0300

    feat: find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if not isinstance(nums[i], int):
+            return False
+        if nums[i] < 1:
+            return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
+
+
+nums = [-1, -1]
+print(find_duplicate(nums))
